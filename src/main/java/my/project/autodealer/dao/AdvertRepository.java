087670f7ed6453181b75eed5194aa0272d97681e
Nikolay <Horushko.Nikolay@gmail.com>
2017-02-04 15:57:22 +0300

    advert map

---
+++ src/main/java/my/project/autodealer/dao/AdvertRepository.java
@@ -0,0 +1,74 @@
+package my.project.autodealer.dao;
+
+import javax.persistence.*;
+
+/**
+ * Created by Nikol on 2/4/2017.
+ */
+public class AdvertRepository {
+    private int id;
+    private int createdDate;
+    private CarRepository car;
+    private UsersRepository users;
+    private RefStatusRepository status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(int createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public CarRepository getCar() {
+        return car;
+    }
+
+    public void setCar(CarRepository car) {
+        this.car = car;
+    }
+
+    public UsersRepository getUsers() {
+        return users;
+    }
+
+    public void setUsers(UsersRepository users) {
+        this.users = users;
+    }
+
+    public RefStatusRepository getStatus() {
+        return status;
+    }
+
+    public void setStatus(RefStatusRepository status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AdvertRepository that = (AdvertRepository) o;
+
+        if (id != that.id) return false;
+        if (createdDate != that.createdDate) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + createdDate;
+        return result;
+    }
+}
