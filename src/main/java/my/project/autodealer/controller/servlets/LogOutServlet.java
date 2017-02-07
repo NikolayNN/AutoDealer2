eec408c864789ec5cce94778acd595ac6169d554
Nikolay <Horushko.Nikolay@gmail.com>
2017-02-07 20:48:47 +0300

    log out

---
+++ src/main/java/my/project/autodealer/controller/servlets/LogOutServlet.java
@@ -0,0 +1,21 @@
+package my.project.autodealer.controller.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Nikol on 2/7/2017.
+ */
+public class LogOutServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().setAttribute("user", null);
+        response.sendRedirect("index.html");
+    }
+}
